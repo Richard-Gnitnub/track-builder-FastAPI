692ea78e63128fe903d763f3dadc3ca70862ef75
RicardoTinbuns <richardbunting3370@hotmail.co.uk>
2024-12-27 13:25:11 +0000

    migration complete env.py updated

--- alembic/env.py
+++ alembic/env.py
@@ -6,6 +6,7 @@ from sqlalchemy import pool
 from alembic import context
 from sqlmodel import SQLModel
 from track_app.models import Track, Timber, Chair  # Import models from your project
+import sqlmodel
 
 # This is the Alembic Config object, which provides
 # access to the values within the .ini file in use.
